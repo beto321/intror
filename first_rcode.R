32ab5668fa3bb62e7512cd4d2c1b9f5c4c1a60a3
beto321 <ivoalabe@gmail.com>
2021-04-24 23:14:20 -0400

    addes code from page 8

--- first_rcode.R
+++ first_rcode.R
@@ -16,4 +16,14 @@ typos.draft2=c(0,3,0,3,1,0,0,1)
 typos.draft2=typos.draft1
 typos.draft2[1]=0
 typos.draft2
+
+typos.draft2[c(1,2,3)]
+max(typos.draft2)
+typos.draft2==3
+which(typos.draft2==3)
+
+sum(typos.draft2)
+sum(typos.draft2>0)
+typos.draft1-typos.draft2
+
     
